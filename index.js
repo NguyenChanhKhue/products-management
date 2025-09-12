8fddf8873d9f84080c37a29554c1a2f0af2206a6
NguyenChanhKhue <chanhkhue7122005@gmail.com>
2025-09-12 12:48:19 +0700

    env

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express')
-
+require('dotenv').config();
 const route = require("./routes/client/index.route")
 const routeAdmin = require("./routes/admin/index.route")
 
 
 const app = express()
-const port = 3000
+const port = process.env.PORT
 
 app.set('views', './views')
 app.set('view engine', 'pug')
