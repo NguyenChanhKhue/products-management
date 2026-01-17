c4ee0fbfdec0f9a5a507998b89f97cdfb8381487
NguyenChanhKhue <chanhkhue7122005@gmail.com>
2026-01-18 00:05:17 +0700

    Hoàn thành phần detail , edit cho Role

--- controllers/admin/role.controller.js
+++ controllers/admin/role.controller.js
@@ -44,4 +44,54 @@ module.exports.createPost =  async (req, res) =>{
 }
 
 
+// [GET] /admin/roles/detail/:id
+module.exports.detail = async (req , res ) => {
+  try {
+    const find = {
+    deleted: false ,
+    _id: req.params.id,
+  }
+
+    const role = await Role.findOne(find);
+
+    console.log(role)
+
+    res.render("admin/pages/role/detail",{
+        pageTitle:"Trang chi tiết nhóm quyền",
+        role:role
+    })
+  } catch (error) {
+    res.redirect("roles")
+  }
+}
+
+// [GET] /admin/roles/edit/:id
+module.exports.edit = async (req , res) =>{
+  const find = {
+    deleted: false ,
+    _id: req.params.id,
+  }
+
+  const role = await Role.findOne(find);
+
+  //console.log(role)
+  res.render("admin/pages/role/edit" , {
+    pageTitle : "Trang chỉnh sửa nhóm quyền",
+    role : role
+  })
+}
+
+// [PATCH] /admin/roles/edit/:id
+module.exports.editPatch = async (req , res) =>{
+  try{
+    await Role.updateOne(
+      {_id:req.params.id},req.body
+    )
+  }catch(error){
+    res.redirect(`${systemConfig.prefixAdmin}/roles/edit/${req.params.id}`)
+  }
+  res.redirect(`${systemConfig.prefixAdmin}/roles`)
+}
+
+
  
--- routes/admin/role.route.js
+++ routes/admin/role.route.js
@@ -9,4 +9,14 @@ router.get('/',controller.roles)
 router.get('/create' , controller.create)
 router.post('/create' , controller.createPost)
 
+// chinh sửa nhóm quyền
+router.get('/edit/:id', controller.edit)
+router.patch('/edit/:id', controller.editPatch)
+
+
+
+
+// chi tiet nhom quyen
+router.get('/detail/:id', controller.detail)
+
 module.exports = router;
